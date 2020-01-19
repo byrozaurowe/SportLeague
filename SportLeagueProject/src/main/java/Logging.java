4601581cdb1b0f1194241fde7fd871b04defc0b1
Wojciech Pakulski <wojtekpakulski@gmail.com>
2020-01-19 13:10:56 +0100

    pusty projekt

--- SportLeagueProject/src/main/java/Logging.java
+++ SportLeagueProject/src/main/java/Logging.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Logging {
+
+    /** Panel głowny */
+    JPanel panel;
+    /** Panel pomocniczy na logowanie */
+    JPanel panel1;
+    /** 
+    JLabel headerLabel;
+    JLabel passwordLabel;
+    JLabel loginLabel;
+
+    public Logging() {
+
+    }
 }
